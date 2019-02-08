74bdc1f6cdeb42089821e78fe589314a275c9642
Alon Rotem <arotem@kpmg.com>
2019-02-08 15:44:37 +0200

    git experiments

--- api/github-jekyll.js
+++ api/github-jekyll.js
@@ -1,22 +1,49 @@
-const git = require("git");
-//var sys = require("sys");
-var exec = require("child_process").exec;
+require("dotenv").config();
+var fs = require("fs-extra");
+const path = require("path");
 
 module.exports = function(app) {
   app.get("/api/github-jekyll/fixunhiddendate", (req, res) => {
-    function puts(error, stdout, stderr) {
-      //sys.puts(stdout);
-      res.write(stdout);
-      res.end();
+    const localPath = path.join(__dirname, "tmp");
+
+    if (fs.existsSync(localPath)) {
+      fs.removeSync(localPath);
     }
-    exec("git version", puts);
-    /*
-    var x = new git.Repo(
-      "https://alonrotem:r0t3mPa$$@github.com/literaturnirazgovori/literaturnirazgovori.github.io.git",
-      { working_directory: "C:\\" },
-      function() {
-        console.log("rep");
+    fs.ensureDirSync(localPath);
+    const simpleGit = require("simple-git")(localPath);
+    console.log("Cloning...");
+    simpleGit.clone(process.env.JEKYLL_GIT_REPO, localPath, (err, info) => {
+      if (!err) {
+        console.log("Cloned");
+        res.write("cloned.");
+        console.log("Getting last commit ID");
+        simpleGit.revparse(["--short", "HEAD"], (err, info) => {
+          if (!err) {
+            console.log("latest commit ID: " + info);
+            res.write("latest commit ID: " + info);
+            res.end();
+            /*
+            console.log("Getting changed files list");
+            //git log --name-status
+            simpleGit.log(["--name-status"], (err, info) => {
+              console.log(info);
+              res.write(info);
+              res.end();
+              if (!err) {
+              } else {
+                res.write("Failed to get files list for commit");
+                res.end();
+              }
+            });*/
+          } else {
+            res.write("Failed to get latest commit ID: " + err);
+            res.end();
+          }
+        });
+      } else {
+        res.write("Failed to clone: " + err);
+        res.end();
       }
-    );*/
+    });
   });
 };
