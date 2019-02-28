56cef00ac64fe4f07b46c9bbf88388f313bd112d
Alon Rotem <alrotem@gmail.com>
2019-02-28 15:18:44 +0200

    Changing promises to async/await

--- api/github-jekyll.js
+++ api/github-jekyll.js
@@ -10,7 +10,7 @@ module.exports = function(app) {
   app.post(
     "/api/github-jekyll/fixunhiddendate",
     githubsecurity.validateGitHubWebhookSecret,
-    (req, res) => {
+    async (req, res) => {
       res.set({ "content-type": "application/json; charset=utf-8" });
 
       let automatedScriptCommitMessage =
@@ -53,151 +53,117 @@ module.exports = function(app) {
       const simpleGit = require("simple-git/promise")(localPath);
       console.log("Cloning...");
       //step 1: clone
-      return (
-        simpleGit
-          .clone(gitUrl, localPath, [
-            "--single-branch" /*,
+      try {
+        await simpleGit.clone(gitUrl, localPath, [
+          "--single-branch" /*,
             "--branch ",
             targetBranch*/
-          ])
-          .catch(err => {
-            console.log(err);
-            let returnobj = JSON.stringify(
-              {
-                error: true,
-                errrmessage: err.message
-              },
-              null,
-              4
-            );
-            console.log(returnobj);
-            res.send(returnobj);
-            res.end();
-            return false;
-          })
-          //step 2: get the most recent commit details
-          .then(() => {
-            console.log("Cloned. Getting the most recent commit details...");
-            return simpleGit.log(["-1"]);
-          })
-          .then(result => {
-            if (result) {
-              console.log(
-                "Latest commit from " +
-                  result.latest.date +
-                  ", by " +
-                  result.latest.author_name +
-                  "(" +
-                  result.latest.author_email +
-                  "): " +
-                  result.latest.message
-              );
-              successResponse.lastCommitInfo = {
-                date: result.latest.date,
-                authorName: result.latest.author_name,
-                authorEmail: result.latest.author_email,
-                commitMessage: result.latest.message
-              };
-              //Check the last commit message, to avoid loops
-              if (
-                result.latest.message.startsWith(automatedScriptCommitMessage)
-              ) {
-                console.log(
-                  "Previous commit message is similar to the automated script commit message. Skipping actions"
-                );
-                return false;
-              }
-            }
-            return result;
-          })
+        ]);
+      } catch (err) {
+        console.log(err);
+        let returnobj = JSON.stringify(
+          {
+            error: true,
+            errrmessage: err.message
+          },
+          null,
+          4
+        );
+        console.log(returnobj);
+        res.send(returnobj);
+        res.end();
+        return false;
+      }
+      console.log("Cloned. Getting the most recent commit details...");
+
+      //step 2: get the most recent commit details
+      let lastCommit = await simpleGit.log(["-1"]);
+      if (lastCommit) {
+        console.log(
+          "Latest commit from " +
+            lastCommit.latest.date +
+            ", by " +
+            lastCommit.latest.author_name +
+            "(" +
+            lastCommit.latest.author_email +
+            "): " +
+            lastCommit.latest.message
+        );
+        successResponse.lastCommitInfo = {
+          date: lastCommit.latest.date,
+          authorName: lastCommit.latest.author_name,
+          authorEmail: lastCommit.latest.author_email,
+          commitMessage: lastCommit.latest.message
+        };
+        //Check the last commit message, to avoid loops
+        if (
+          !lastCommit.latest.message.startsWith(automatedScriptCommitMessage)
+        ) {
           //step 3: set the repo configuration, to prevent weird octet-encoded file names
-          .then(keeprunning => {
-            simpleGit.addConfig("core.quotepath", "off");
-            return keeprunning;
-          })
-          .then(keeprunning => {
-            simpleGit.raw([
-              "config",
-              "user.email",
-              process.env.JEKYLL_GIT_EMAIL
-            ]);
-            successResponse.committerEmail = process.env.JEKYLL_GIT_EMAIL;
-            return keeprunning;
-          })
-          .then(keeprunning => {
-            simpleGit.raw(["config", "user.name", process.env.JEKYLL_GIT_USER]);
-            successResponse.committerUser = process.env.JEKYLL_GIT_USER;
-            return keeprunning;
-          })
-          .then(keeprunning => {
-            simpleGit.raw(["config", "user.name", process.env.JEKYLL_GIT_USER]);
-            successResponse.committerUser = process.env.JEKYLL_GIT_USER;
-            return keeprunning;
-          })
+          await simpleGit.addConfig("core.quotepath", "off");
+          await simpleGit.raw([
+            "config",
+            "user.email",
+            process.env.JEKYLL_GIT_EMAIL
+          ]);
+          successResponse.committerEmail = process.env.JEKYLL_GIT_EMAIL;
+
+          await simpleGit.raw([
+            "config",
+            "user.name",
+            process.env.JEKYLL_GIT_USER
+          ]);
+          successResponse.committerUser = process.env.JEKYLL_GIT_USER;
+
           //step 4: get the diff files from the latest commit (HEAD) and the previous one (HEAD~1).
           //filter out (a)dded, (c)opied, (d)eleted, (r)enamed, (t)ype-changed, (u)nmerged or (x)-unknown changes.
           //this leaves only content-(m)odified files.
-          .then(keeprunning => {
-            if (keeprunning) {
-              return simpleGit.diff([
-                "--name-only", //only the filenames
-                "--diff-filter=acdrtux", //modified files only
-                "HEAD~1", //from previous commit
-                "HEAD" //to latest version
-              ]);
-            } else {
-              return false;
-            }
-          })
-          .then(result => {
-            if (result) {
-              let files = result.trim().split("\n");
-              if (files) {
-                successResponse.modifiedFiles = files.length;
-                console.log(
-                  "Found " +
-                    files.length +
-                    " modified file(s) in the latest commit."
-                );
-                for (let i = 0; i < files.length; i++) {
-                  let file = files[i].replace(/\"*/g, "");
-                  if (file) {
-                    //current state file: get the content, read the front-matter and check if it's a hidden post.
-                    var y = fs.readFileSync(path.resolve(localPath, file), {
-                      encoding: "utf8"
-                    });
-                    let fm = frontmatter(y.toString());
-                    let isHiddenOnLastCommit = fm.attributes.hidden;
+          let mofifiedFilenames = await simpleGit.diff([
+            "--name-only", //only the filenames
+            "--diff-filter=acdrtux", //modified files only
+            "HEAD~1", //from previous commit
+            "HEAD" //to latest version
+          ]);
 
-                    currentfileinfo.push({
-                      filename: file,
-                      hiddenOnLastCommit: isHiddenOnLastCommit
-                    });
-                    //collect a list of promises for showing previous versions of those files
-                    previousFileVersionsPromises.push(
-                      simpleGit.show(["HEAD~1:" + file])
-                    );
-                  }
+          if (mofifiedFilenames) {
+            let files = mofifiedFilenames.trim().split("\n");
+            if (files) {
+              successResponse.modifiedFiles = files.length;
+              console.log(
+                "Found " +
+                  files.length +
+                  " modified file(s) in the latest commit."
+              );
+              for (let i = 0; i < files.length; i++) {
+                let file = files[i].replace(/\"*/g, "");
+                if (file) {
+                  //current state file: get the content, read the front-matter and check if it's a hidden post.
+                  var y = fs.readFileSync(path.resolve(localPath, file), {
+                    encoding: "utf8"
+                  });
+                  let fm = frontmatter(y.toString());
+                  let isHiddenOnLastCommit = fm.attributes.hidden;
+
+                  currentfileinfo.push({
+                    filename: file,
+                    hiddenOnLastCommit: isHiddenOnLastCommit
+                  });
+                  //collect a list of promises for showing previous versions of those files
+                  previousFileVersionsPromises.push(
+                    simpleGit.show(["HEAD~1:" + file])
+                  );
                 }
               }
             }
-            //move the result ahead, if it's falsy, the next promise handler should also not do anything
-            return result;
-          })
-          //let all the promises run, to get details of the modified files and their previous versions
-          .then(result => {
-            if (result) {
-              return Promise.all(previousFileVersionsPromises);
-            } else {
-              return false;
-            }
-          })
-          //step 5: check the modified files in their previous version, their frontmatter, and their hidden status
-          .then(results => {
+            let previousFileVersionContents = await Promise.all(
+              previousFileVersionsPromises
+            );
+
+            //step 5: check the modified files in their previous version, their frontmatter, and their hidden status
             successResponse.changedfiles = [];
-            if (results) {
-              for (let r = 0; r < results.length; r++) {
-                let previousversion = results[r];
+            if (previousFileVersionContents) {
+              for (let r = 0; r < previousFileVersionContents.length; r++) {
+                let previousversion = previousFileVersionContents[r];
                 let fm = frontmatter(previousversion);
                 let isHiddenOnPreviousCommit = fm.attributes.hidden;
                 //if the file was hidden before the latest commit, then unhidden on the last commit, rename its date to today+now
@@ -252,7 +218,7 @@ module.exports = function(app) {
                     localPath,
                     filepath + filename.replace(/^[\d\-]*/g, nowtring)
                   );
-                  //-------------------------
+
                   let oldfilecontent = fs.readFileSync(oldfilename).toString();
                   let oldfileFrontmatter = frontmatter(oldfilecontent)
                     .attributes;
@@ -303,65 +269,46 @@ module.exports = function(app) {
                     });
                   }
                   gitMvRenamedFilesPromises.push(
+                    simpleGit.raw(["add", "-A", newfilename])
+                  );
+                  gitMvRenamedFilesPromises.push(
                     simpleGit.raw(["rm", "--cached", oldfilename])
                   );
-                  gitMvRenamedFilesPromises.push(simpleGit.add(newfilename));
-                  /* replace front matter
-                  previousversion.replace(/(^\-{3,}\n)[\S\s]*(\n\-{3,})/, "$1OK$2")
-                  */
                 }
               }
+              //end of loop
+              let fileschanged = gitMvRenamedFilesPromises.length > 0;
+              console.log(
+                "Files changed:" +
+                  fileschanged +
+                  ", Promises (remove/add) to perform: " +
+                  gitMvRenamedFilesPromises.length
+              );
+              if (fileschanged) {
+                console.log(
+                  "Awaiting promises for add/remove files from the repo..."
+                );
+                await Promise.all(gitMvRenamedFilesPromises);
+
+                await simpleGit.commit(automatedScriptCommitMessage);
+                successResponse.comitted = true;
+                successResponse.commitmessage = automatedScriptCommitMessage;
+
+                await simpleGit.push("origin", targetBranch);
+                successResponse.pushed = true;
+              }
             }
-          })
-          .then(() => {
-            let fileschanged = gitMvRenamedFilesPromises.length > 0;
-            console.log(
-              "Files changed:" +
-                fileschanged +
-                ", Promises (remove/add) to perform: " +
-                gitMvRenamedFilesPromises.length
-            );
-            if (fileschanged) {
-              console.log("Awaiting promises...");
-              return Promise.all(gitMvRenamedFilesPromises);
-            } else {
-              return fileschanged;
-            }
-          })
-          /*
-          .then(keeprunning => {
-            fileschanged =
-              successResponse.changedfiles &&
-              successResponse.changedfiles.length > 0;
-            if (keeprunning && fileschanged) {
-              simpleGit.add("./*");
-              successResponse.added = true;
-            }
-            return keeprunning && fileschanged;
-          })
-          */
-          .then(keeprunning => {
-            if (keeprunning) {
-              simpleGit.commit(automatedScriptCommitMessage);
-              successResponse.comitted = true;
-              successResponse.commitmessage = automatedScriptCommitMessage;
-            }
-            return keeprunning;
-          })
-          .then(keeprunning => {
-            if (keeprunning) {
-              simpleGit.push("origin", targetBranch);
-              successResponse.pushed = true;
-            }
-            return keeprunning;
-          })
-          .then(() => {
-            let returnobj = JSON.stringify(successResponse, null, 4);
-            console.log(returnobj);
-            res.send(returnobj);
-            res.end();
-          })
-      );
+          }
+        } else {
+          console.log(
+            "Previous commit message is similar to the automated script commit message. Skipping actions"
+          );
+        }
+        let returnobj = JSON.stringify(successResponse, null, 4);
+        console.log(returnobj);
+        res.send(returnobj);
+        res.end();
+      }
     }
   );
 };
